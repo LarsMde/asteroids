7594a4b82d2bd2be6236787ff50c85eb2c2013fb
Lars Mueller <LarsM@gmx.net>
2025-02-14 08:25:14 +0100

    test

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame   
 from constants import *
 
 def main():
     pygame.init()
+    pygame.time.Clock = 0
+    dt = 0
     print("Starting asteroids!")
 
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,7 +22,9 @@ def main():
                 return
         
         screen.fill((0,0,0), rect=None, special_flags=0)
-        pygame.display.flip()  
+        pygame.display.flip()
+        pygame.time.Clock.tick(60)
+        dt = pygame.time.Clock.get_time()/1000  
     
 if __name__ == "__main__":
     main()
